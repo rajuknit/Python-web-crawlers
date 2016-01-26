b0f65630ef59635500f1f09087dd8388b650de8c
rajuknit <jalesar.raju1@gmail.com>
2016-01-27 00:46:51 +0530

    Create StackOverFlowQuestions.py

---
+++ StackOverFlowQuestions.py
@@ -0,0 +1,21 @@
+import os
+import lxml.html
+from lxml import etree
+import urllib
+from collections import defaultdict as dd
+'''
+    Author-- Raju Varshney 13228
+'''
+d=dd(lambda:0)
+
+   
+url="http://stackoverflow.com/questions?pagesize=50&sort=newest"
+#print(url)
+tree = lxml.html.parse(url)
+questions = tree.xpath('//div[@class="summary"]/h3')  
+for question in questions:
+            #print(question)
+            print(question.xpath('a[@class="question-hyperlink"]/text()'))
+            print(question.xpath('a[@class="question-hyperlink"]/@href'))
+          
+    
